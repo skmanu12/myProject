fa7c56677bcc7a7b3d399f89109f9235880ddeb5
skmanu12 <121294092+skmanu12@users.noreply.github.com>
2023-11-03 12:08:36 +0530

    Update and rename Locations.java to CheckOutEntryVehiclesInfoGuestUser.java

---
+++ src/main/java/restAssured/PojoClasses/copy/CheckOutEntryVehiclesInfoGuestUser.java
@@ -0,0 +1,86 @@
+package pojoClasses;
+
+public class CheckOutEntryVehiclesInfoGuestUser {
+	private String vehicleType;
+	private String licensePlate;
+	private String licensedState;
+	private String make;
+	private String model;
+	private String color;
+	private String alias;
+	private String isDefault;
+	private int numberOfPassengers;
+
+	public String getVehicleType() {
+		return vehicleType;
+	}
+
+	public void setVehicleType(String vehicleType) {
+		this.vehicleType = vehicleType;
+	}
+
+	public String getLicensePlate() {
+		return licensePlate;
+	}
+
+	public void setLicensePlate(String licensePlate) {
+		this.licensePlate = licensePlate;
+	}
+
+	public String getLicensedState() {
+		return licensedState;
+	}
+
+	public void setLicensedState(String licensedState) {
+		this.licensedState = licensedState;
+	}
+
+	public String getMake() {
+		return make;
+	}
+
+	public void setMake(String make) {
+		this.make = make;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+
+	public String isDefault() {
+		return isDefault;
+	}
+
+	public void setDefault(String isDefault2) {
+		this.isDefault = isDefault2;
+	}
+
+	public int getNumberOfPassengers() {
+		return numberOfPassengers;
+	}
+
+	public void setNumberOfPassengers(int numberOfPassengers) {
+		this.numberOfPassengers = numberOfPassengers;
+	}
+
+}
