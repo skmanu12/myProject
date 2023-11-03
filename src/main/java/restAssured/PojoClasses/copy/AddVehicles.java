4ffa0e3f3c2827033e41368f76bebe0238cb602b
skmanu12 <121294092+skmanu12@users.noreply.github.com>
2023-11-03 12:05:44 +0530

    Update and rename AddPlace.java to AddVehicles.java

---
+++ src/main/java/restAssured/PojoClasses/copy/AddVehicles.java
@@ -0,0 +1,66 @@
+package pojoClasses;
+
+public class AddVehicles {
+private String vehicleType;
+private String licensePlate;
+private String licensedState;
+private String make;
+private String model;
+private String color;
+private String numberOfPassengers;
+private String alias;
+
+
+
+public String getVehicleType() {
+	return vehicleType;
+}
+public void setVehicleType(String vehicleType) {
+	this.vehicleType = vehicleType;
+}
+public String getLicensePlate() {
+	return licensePlate;
+}
+public void setLicensePlate(String licensePlate) {
+	this.licensePlate = licensePlate;
+}
+public String getLicensedState() {
+	return licensedState;
+}
+public void setLicensedState(String licensedState) {
+	this.licensedState = licensedState;
+}
+public String getMake() {
+	return make;
+}
+public void setMake(String make) {
+	this.make = make;
+}
+public String getModel() {
+	return model;
+}
+public void setModel(String model) {
+	this.model = model;
+}
+public String getColor() {
+	return color;
+}
+public void setColor(String color) {
+	this.color = color;
+}
+public String getNumberOfPassengers() {
+	return numberOfPassengers;
+}
+public void setNumberOfPassengers(String numberOfPassengers) {
+	this.numberOfPassengers = numberOfPassengers;
+}
+public String getAlias() {
+	return alias;
+}
+public void setAlias(String alias) {
+	this.alias = alias;
+}
+
+
+
+}
