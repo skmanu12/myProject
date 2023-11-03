96c0bf635233ea648748284309f71c5117612f22
skmanu12 <121294092+skmanu12@users.noreply.github.com>
2023-11-03 12:12:16 +0530

    Update and rename OrderDetails.java to CheckOutPaymentRegisteredUser.java

---
+++ src/main/java/restAssured/PojoClasses/copy/CheckOutPaymentRegisteredUser.java
@@ -0,0 +1,13 @@
+package pojoClasses;
+
+public class CheckOutPaymentRegisteredUser {
+private String id;
+
+public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
+}
