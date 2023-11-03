1b92135891893d120dff53ed32a3c44cd5bacc24
skmanu12 <121294092+skmanu12@users.noreply.github.com>
2023-11-03 11:37:32 +0530

    Update TestRunner.java

--- src/test/java/CucucmberOptons/TestRunner.java
+++ src/test/java/CucucmberOptons/TestRunner.java
@@ -5,9 +5,23 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
+//Master
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/java/features/LoginPage.feature",
-glue="stepDefinations")
+@CucumberOptions(
+       features = {"src/test/java/features/SignAccount.feature"}, 
+       plugin = {"json:target/jsonReports/cucumber-report.json",
+		         "rerun:target/failedrerun.txt"}, 
+       glue = { "stepDefinations" }
+      )
+ //FailedtestCases execution test runner class
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = { "@target/failedrerun.txt" }, 
+        plugin = { "json:target/jsonReports/cucumber-report.json",
+		"rerun:target/failedrerun.txt" }, 
+        glue = { "stepDefinations" })
+
+ 
 public class TestRunner {
 
 }
