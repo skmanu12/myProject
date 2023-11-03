ad7b565351d50a904a8841edceaa6c971b3600f2
skmanu12 <121294092+skmanu12@users.noreply.github.com>
2023-11-03 12:26:13 +0530

    Update and rename WebAutomation.java to NewOrdersEntryRegisteredUser.java

---
+++ src/main/java/restAssured/PojoClasses/copy/NewOrdersEntryRegisteredUser.java
@@ -0,0 +1,34 @@
+package pojoClasses;
+
+public class NewOrdersEntryRegisteredUser {
+private int locationCode;
+private int quantity;
+private String startAt;
+private String stopAt;
+
+public int getLocationCode() {
+	return locationCode;
+}
+public void setLocationCode(int locationCode) {
+	this.locationCode = locationCode;
+}
+public int getQuantity() {
+	return quantity;
+}
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
+public String getStartAt() {
+	return startAt;
+}
+public void setStartAt(String startAt) {
+	this.startAt = startAt;
+}
+public String getStopAt() {
+	return stopAt;
+}
+public void setStopAt(String stopAt) {
+	this.stopAt = stopAt;
+}
+
+}
